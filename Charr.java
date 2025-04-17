6889c5d9ba4b6794751011710de64faf6d09fe3d
LakshyanAlahari <alahari.lakshyan@gmail.com>
2025-04-17 09:23:45 +0530

    using FileReader

---
+++ Charr.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+public class Charr{
+    public static void main(String[] args){
+        try{
+            FileReader fr =  new FileReader("unit.txt");
+            int ch;
+        while((ch = fr.read()) != -1) {
+            System.out.println((char) ch);
+        }
+        fr.close();
+    }catch(IOException e){
+        System.out.println("An error occurred while reading the file: " +e.getMessage());
+        }
+    }
+}
